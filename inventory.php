a7862d9243018ad031b41362e720fce818023104
Jonas Palmiano <palmianojonas@gmail.com>
2022-04-06 13:01:41 +0800

    CHANGED

--- inventory.php
+++ inventory.php
@@ -1,16 +1,46 @@
 <!DOCTYPE html>
 <head><title>Inventory</title>
-<?php include "head.php" ?>
+<?php include "head.php";
+      include "sheets.php";
+?>
 </head>
 <body>
 <div class="navbar">
 <h4 style="color:white;">Gemotech Inc.</h4>
 </div>
+
+</div>
 <?php
-
-
-
+$range ="Details!A2:G20";
+$response = $service ->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
 ?>
 
+<table class="table" style="width: 55%;"> 
+  <tr>
+    <th style=" text-align:center;">Action</th>
+    <th style=" text-align:center;">Id</th>
+    <th style=" text-align:center;">Description</th>
+    <th style=" text-align:center;">Age</th>
+    <th style=" text-align:center;">Gender</th>
+    <th style=" text-align:center;">Location</th>
+    <th style=" text-align:center;">Nationality</th>
 
+</tr>
+
+<?php
+if(empty($values)) {
+    print "No data found .\n";
+} else {
+    //$mask = "%10s %-10s %s";
+    foreach ($values as $row){
+        echo "<tr style='text-align:center;'><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td><td>" . $row[6] . "</td></tr>";
+        }
+}
+?>
+</table>
 </body>
+
+
+
+
--- sheets.php
+++ sheets.php
@@ -9,16 +9,5 @@ $client->setAuthConfig(__DIR__ . '/credentials.json');
 $service = new Google_Service_Sheets($client);
 $spreadsheetId = "1E2q3EC2gWZ6sUydb3oowXjZQ4_eAQJ98Z1LDvkeEXBQ";
 
-$range ="Details!D2:F6";
-$response = $service ->spreadsheets_values->get($spreadsheetId, $range);
-$values = $response->getValues();
-if(empty($values)) {
-    print "No data found .\n";
-} else {
-    $mask = "%10s %-10s %s\n";
-    foreach ($values as $row){
-        echo sprintf($mask, $row[2], $row[1], $row[0]);
-    }
-}
 
 ?>
