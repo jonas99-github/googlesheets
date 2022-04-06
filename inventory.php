646738ea81623895673b2bf04d4c9a515ed63e24
Jonas Palmiano <palmianojonas@gmail.com>
2022-04-06 14:49:37 +0800

    changed

--- inventory.php
+++ inventory.php
@@ -6,10 +6,15 @@
 </head>
 <body>
 <div class="navbar">
-<h4 style="color:white;">Gemotech Inc.</h4>
-</div>
+
+<h4 style="color:white;">Gemotech</h4>
+
+
+<input type="text" name="search" id="search">
+<input type="submit" name="search" value="search" class="btn btn-primary">
 
 </div>
+
 <?php
 $range ="Details!A2:G20";
 $response = $service ->spreadsheets_values->get($spreadsheetId, $range);
@@ -21,6 +26,7 @@ $values = $response->getValues();
     <th style=" text-align:center;">Action</th>
     <th style=" text-align:center;">Id</th>
     <th style=" text-align:center;">Description</th>
+    <th style=" text-align:center;">Email</th>
     <th style=" text-align:center;">Age</th>
     <th style=" text-align:center;">Gender</th>
     <th style=" text-align:center;">Location</th>
@@ -34,7 +40,7 @@ if(empty($values)) {
 } else {
     //$mask = "%10s %-10s %s";
     foreach ($values as $row){
-        echo "<tr style='text-align:center;'><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td><td>" . $row[6] . "</td></tr>";
+        echo "<tr style='text-align:center;'><td><button class='btn btn-primary'>View</button></td><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td><td>" . $row[6] . "</td></tr>";
         }
 }
 ?>
