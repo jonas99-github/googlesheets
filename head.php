ff087895b618e257a8804ec4488d5ebcb2fe131e
Jonas Palmiano <palmianojonas@gmail.com>
2022-04-16 14:56:16 +0800

    ouhyiu

--- head.php
+++ head.php
@@ -1,2 +1,6 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="./css/main.css">
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- inventory.php
+++ inventory.php
@@ -3,10 +3,11 @@
 <?php include "head.php";
       include "sheets.php";
 ?>
+
 </head>
 <body>
 <div class="navbar">
-<h4 style="color:white;">Nav</h4>
+<h4 style="color:white;">Gemotech Inc.</h4>
 
 
 <!--<input type="text" name="search" id="search">
@@ -15,7 +16,7 @@
 </div>
 
 <?php 
-$range ="test!A2:C24";
+$range ="test!A2:J24";
 $response = $service ->spreadsheets_values->get($spreadsheetId, $range);
 $values = $response->getValues(); 
 ?>
@@ -24,24 +25,25 @@ $values = $response->getValues();
   <tr>
     <th style=" text-align:center;">Action</th>
     <th style=" text-align:center;">Id</th>
+    <th style=" text-align:center;">Item</th>
     <th style=" text-align:center;">Brand</th>
     <th style=" text-align:center;">Model</th>
-    <th style=" text-align:center;">Specs</th>
     <th style=" text-align:center;">Tag</th>
     <th style=" text-align:center;">Serial number</th>
-    <th style=" text-align:center;">Employee Name</th>
     <th style=" text-align:center;">Department</th>
+    <th style=" text-align:center;">Employee Name</th>
     <th style=" text-align:center;">Note</th>
     <th style=" text-align:center;">Date</th>
     <th style=" text-align:center;">Location</th>
-</tr>
+  </tr>
 
 <?php
 if(empty($values)) {
     print "No data found .\n";
 } else {
     //$mask = "%10s %-10s %s";
-    $i = 0;
+    //$i = 0;
+    /*
     while($i<count($values)){
         echo "
         <tr style='text-align:center;'>
@@ -51,19 +53,84 @@ if(empty($values)) {
         </tr>";
         }
     }
-    /*
-    foreach ($values as $row){
-        echo "
-        <tr style='text-align:center;'>
-        <td><button class='btn btn-primary'>Update</button><button class='btn btn-danger'>Delete</button></td>
-        <td>" . $row[0] ?? 'default' . "</td>
-        <td>" . $row[1] ?? 'default' . "</td>
-        <td>" . $row[2] ?? 'default' . "</td>
+    */
+    
+    foreach ($values as $col){
+$res0=$col[0] ?? "";
+$res1=$col[1] ?? "";
+$res2=$col[2] ?? "";
+$res3=$col[3] ?? "default";
+$res4=$col[4] ?? "";
+$res5=$col[5] ?? "";
+$res6=$col[6] ?? "";
+$res7=$col[7] ?? "";
+$res8=$col[8] ?? "";
+$res9=$col[9] ?? "";
+
+
+        echo "<tr style='text-align:center;'>
+        <td><button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop'>Update</button>
+        <button class='btn btn-danger'>Delete</button></td>
+        <td></td>
+        <td>" . $res0  . "</td>
+        <td>" . $res1  . "</td>
+        <td>" . $res2  . "</td>
+        <td>" . $res3  . "</td>
+        <td>" . $res4  . "</td>
+        <td>" . $res5  . "</td>
+        <td>" . $res6  . "</td>
+        <td>" . $res7  . "</td>
+        <td>" . $res8  . "</td>
+        <td>" . $res9  . "</td>
         </tr>";
         }
-    */
-
+    
+    }
 ?>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Update Item</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="" >
+            <label>Id</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Item</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Brand</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Model</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Tag</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Serial number</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Department</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Employee name</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Note</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Date</label><br>
+            <input type="text" id="" name="" required><br>
+            <label>Location</label><br>
+            <input type="text" id="" name="" required><br>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
 </table>
 </body>
 
